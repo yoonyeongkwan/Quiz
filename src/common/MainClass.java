7f551d48617f45d93c17e22561024cf5ebd4e324
메이플 <maple@Naver.com>
2024-02-16 15:22:14 +0900

    메인 플젝 생성

---
+++ src/common/MainClass.java
@@ -0,0 +1,21 @@
+package common;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int choice;
+		System.out.println("main project 시작");
+		System.out.println("1.에어컨 2.리모컨 3.계산기");
+		choice = sc.nextInt();
+		switch(choice) {
+		case 1 : break;
+		case 2 : break;
+		case 3 : break;
+		}
+	}
+
+}
